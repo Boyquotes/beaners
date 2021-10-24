306f5f3d17c357685185dc88bfb44d19480e7bb4
GianXD <borcillofg2020@gmail.com>
2021-10-24 22:02:17 +0800

    feat: UI improvements

--- Script/ui/MainMenu.gd
+++ Script/ui/MainMenu.gd
@@ -1,8 +1,9 @@
 extends MarginContainer
 
-onready var selector_play = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/PlaySelector"
-onready var selector_options = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/OptionsSelector" 
-onready var selector_quit = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/QuitSelector"
+onready var play = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Play"
+onready var options = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Options"
+onready var help = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer4/HBoxContainer/Help"
+onready var quit = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Quit"
 onready var navigate_sfx = $"NavigateSfx"
 onready var select_sfx = $"SelectSfx"
 onready var back_sfx = $"BackSfx"
@@ -14,35 +15,42 @@ func _ready():
 	update_current_selection()
 
 func _process(_delta):
+	if (Input.is_action_just_pressed("ui_up") and current_selection > 0
+	   or Input.is_action_just_pressed("ui_down") and current_selection < 3):
+		navigate_sfx.play()
+	
 	if Input.is_action_just_pressed("ui_up") and current_selection > 0:
 		current_selection -= 1
 		update_current_selection()
-	elif Input.is_action_just_pressed("ui_down") and current_selection < 2:	
+	elif Input.is_action_just_pressed("ui_down") and current_selection < 3:
 		current_selection += 1
 		update_current_selection()
 	elif Input.is_action_just_pressed("ui_accept"):
+		select_sfx.play()
 		handle_selection()
 
 func handle_selection():
-	select_sfx.play()
-	
 	if current_selection == 0:
 		var _scene = get_tree().change_scene("res:///Scene/worlds/Void.tscn")
 	elif current_selection == 1:
 		print("Options coming soon!")
 	elif current_selection == 2:
+		print("Help coming soon!")
+	elif current_selection == 3:
 		get_tree().quit()
 
 func update_current_selection():
-	navigate_sfx.play()
 	var selection = current_selection
-	selector_play.set_text("")
-	selector_options.set_text("")
-	selector_quit.set_text("")
+	play.set_scale(Vector2(1, 1))
+	options.set_scale(Vector2(1, 1))
+	help.set_scale(Vector2(1, 1))
+	quit.set_scale(Vector2(1, 1))
 	
 	if selection == 0:
-		selector_play.set_text(">")
+		play.set_scale(Vector2(1.2, 1.2))
 	elif selection == 1:
-		selector_options.set_text(">")
+		options.set_scale(Vector2(1.2, 1.2))
 	elif selection == 2:
-		selector_quit.set_text(">")
+		help.set_scale(Vector2(1.2, 1.2))
+	elif selection == 3:
+		quit.set_scale(Vector2(1.2, 1.2))
--- Script/ui/PauseMenu.gd
+++ Script/ui/PauseMenu.gd
@@ -1,10 +1,10 @@
 extends MarginContainer
 
 onready var world = $"../../"
-onready var selector_resume = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/ResumeSelector"
-onready var selector_options = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/OptionsSelector" 
-onready var selector_mmenu = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer4/HBoxContainer/MMenuSelector"
-onready var selector_quit = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/QuitSelector"
+onready var resume = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Resume"
+onready var options = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Options" 
+onready var mmenu = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer4/HBoxContainer/MMenu"
+onready var quit = $"CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Quit"
 onready var navigate_sfx = $"NavigateSfx"
 onready var select_sfx = $"SelectSfx"
 onready var back_sfx = $"BackSfx"
@@ -15,6 +15,10 @@ func _ready():
 	update_current_selection()
 
 func _process(_delta):
+	if (Input.is_action_just_pressed("ui_up") and current_selection > 0
+	   or Input.is_action_just_pressed("ui_down") and current_selection < 3):
+		navigate_sfx.play()
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		back_sfx.play()
 		current_selection = 0
@@ -27,11 +31,10 @@ func _process(_delta):
 		current_selection += 1
 		update_current_selection()
 	elif Input.is_action_just_pressed("ui_accept"):
+		select_sfx.play()
 		handle_selection()
 
 func handle_selection():
-	select_sfx.play()
-	
 	if current_selection == 0:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		get_node("/root/World/Player/Head").set_visible(true)
@@ -47,17 +50,16 @@ func handle_selection():
 
 func update_current_selection():
 	var selection = current_selection
-	selector_resume.set_text("")
-	selector_options.set_text("")
-	selector_mmenu.set_text("")
-	selector_quit.set_text("")
-	navigate_sfx.play()
+	resume.set_scale(Vector2(1, 1))
+	options.set_scale(Vector2(1, 1))
+	mmenu.set_scale(Vector2(1, 1))
+	quit.set_scale(Vector2(1, 1))
 	
 	if selection == 0:
-		selector_resume.set_text(">")
+		resume.set_scale(Vector2(1.2, 1.2))
 	elif selection == 1:
-		selector_options.set_text(">")
+		options.set_scale(Vector2(1.2, 1.2))
 	elif selection == 2:
-		selector_mmenu.set_text(">")
+		mmenu.set_scale(Vector2(1.2, 1.2))
 	elif selection == 3:
-		selector_quit.set_text(">")
+		quit.set_scale(Vector2(1.2, 1.2))

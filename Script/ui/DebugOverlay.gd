4036d3fe8cfb8ba48501d8c9b83749414c5a9840
GianXD <borcillofg2020@gmail.com>
2021-10-25 12:53:19 +0800

    feat: More debug information, feat: Toggle debug info

--- Script/ui/DebugOverlay.gd
+++ Script/ui/DebugOverlay.gd
@@ -12,13 +12,21 @@ func add_statistics(stats_name, object, stats_reference, is_method):
 
 func _process(_delta):
 	var fps = Engine.get_frames_per_second()
+	var os = OS.get_name()
 	var dynamic_memory = OS.get_dynamic_memory_usage()
 	var static_memory = OS.get_static_memory_usage()
+	var graphics_driver = OS.get_video_driver_name(OS.get_current_video_driver())
+	var sound_driver = OS.get_audio_driver_name(OS.get_current_video_driver())
+	var window_size = OS.get_window_size()
 	var new_label = ""
 	
 	new_label += str("FPS: ", fps, "\n")
+	new_label += str("Platform: ", os, "\n")
 	new_label += str("Dynamic Memory: ", String.humanize_size(dynamic_memory), "\n")
 	new_label += str("Static Memory: ", String.humanize_size(static_memory), "\n")
+	new_label += str("Graphics driver: ", graphics_driver, "\n")
+	new_label += str("Sound driver: ", sound_driver, "\n")
+	new_label += str("Resolution: ", window_size, "\n")
 	
 	for value in statistics:
 		var new_value = null
---
+++ Script/worlds/Void.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+onready var debug_overlay_label = $"DebugOverlay/Label"
+
+func _init():
+	if not OS.is_debug_build():
+		debug_overlay_label.hide()
+
+func _process(_delta):
+	if Input.is_action_pressed("ui_debug"):
+		if not debug_overlay_label.is_visible():
+			debug_overlay_label.show()
+		else:
+			debug_overlay_label.hide()

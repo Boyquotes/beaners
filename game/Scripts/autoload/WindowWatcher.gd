8809635b39b0ef6b98de0cfb688e5bc91d7cd093
Gian Borcillo <borcillofg2020@gmail.com>
2022-08-18 12:21:51 +0800

    fix: Prevent graphics config from saving when maximized or fullscreen

--- game/Scripts/autoload/WindowWatcher.gd
+++ game/Scripts/autoload/WindowWatcher.gd
@@ -11,6 +11,9 @@ func _ready():
 func _size_changed():
 	# This function is called when root's size_changed is emitted
 	# and will save the modified window size
+	if OS.is_window_maximized() or OS.is_window_fullscreen():
+		return
+	
 	var config = ConfigWatcher.get_graphics_config()
 	var root = get_tree().get_root()
 	config.set_maximize(OS.is_window_maximized())
